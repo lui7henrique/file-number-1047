335172113c896af5db7413933b19a5c4c7701dda
Luiz Henrique <7henrique18@gmail.com>
2022-10-07 00:05:12 -0300

    commit e.e

---
+++ script.js
@@ -0,0 +1,18 @@
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const elementClasses = ["first", "second", "third", "fourth"];
+
+elementClasses.forEach(async (elementClass, index) => {
+  const el = document.getElementById(elementClass);
+  const delay = index * 3000;
+
+  setTimeout(() => {
+    el.style.display = "block";
+  }, delay);
+
+  if (index === elementClasses.length - 1) {
+    console.log("fecha");
+  }
+});
